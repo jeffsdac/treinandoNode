fb9a588b1245e7c1503b3352a513cf7b013a2ab5
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2023-08-29 15:57:19 -0300

    aprendendo sobre funções sincronas e assincronas

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 import chalk from "chalk";
+import fs from 'fs';
 
-console.log(chalk.blue("Hello, world!"));
+function trataErro (erro){
+    throw new Error (chalk.red(erro.code, 'Não há arquivo no diretorio'))
+}
+
+// Async / Await
+async function takeFile (pathFile) {
+    try{
+        const encoding = "utf-8";
+        const texto = await fs.promises.readFile(pathFile, encoding)
+        console.log(chalk.green(texto));
+    }catch (erro){
+        trataErro(erro);
+    }finally{
+        console.log(chalk.yellow("TODOS OS ARQUIVOS FORAM LIDOS"));
+    }
+}
+
+
+// THEN
+
+/**function takeFile (path) {
+    const encoding = 'utf-8';
+    fs.readFile(path, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto))
+    })
+}**/
+
+/**function takeFile(path){
+    const encoding = "utf-8";
+    fs.promises.readFile(path, encoding)
+     .then((texto) => console.log(chalk.green(texto)))
+     .catch(trataErro);
+}**/
+
+takeFile('./arquivos/texto.md');
