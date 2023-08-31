42038bd718298896a86a7b1ca075166661bf0700
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2023-08-31 11:04:22 -0300

    Adicionando a funcionaldiade de ler os arquivos pelo diretorio

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,29 @@
 import chalk from "chalk";
-
+import fs from 'fs';
 import writeFile from "./index.js";
 
 const caminho = process.argv;
 
 async function showFile () {
-    const texto = await writeFile(caminho[2]);
-
-    console.log(chalk.blueBright("O TEXTO PEGO FOI: "), texto);
+    const path = caminho[2];
+    
+    if (fs.lstatSync(path).isFile()){
+        const resultado = await writeFile(path);
+     
+    } 
+    else if (fs.lstatSync(path).isDirectory()){
+        const arquivos = await fs.promises.readdir(path);
+        arquivos.forEach(async (texts) => {
+            const lista = await writeFile(`${path}/${texts}`);
+            showList(lista);
+        })
+        
+    }
+   
 }
+
+function showList (list){
+    console.log(chalk.yellow('Lista de links: '), list);
+} 
 
 showFile();
